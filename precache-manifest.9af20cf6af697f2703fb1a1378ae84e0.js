f497df85ab942b36fce89a9376a72406f7d49f4f
praise adedoyin <adedoyinpraise408@gmail.com>
2022-08-28 14:05:57 +0100

    Updates

---
+++ precache-manifest.9af20cf6af697f2703fb1a1378ae84e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfda7c1483c8d33f7a27",
+    "url": "/Todo-App/static/css/main.3e3ba665.chunk.css"
+  },
+  {
+    "revision": "cfda7c1483c8d33f7a27",
+    "url": "/Todo-App/static/js/main.cfda7c14.chunk.js"
+  },
+  {
+    "revision": "35e0ce2b3cdcc6ec36c5",
+    "url": "/Todo-App/static/js/1.35e0ce2b.chunk.js"
+  },
+  {
+    "revision": "58db14a1c0e0e44501c3",
+    "url": "/Todo-App/static/js/runtime~main.58db14a1.js"
+  },
+  {
+    "revision": "109cbe77fbb0bfc025c11f52185eb6d7",
+    "url": "/Todo-App/index.html"
+  }
+];
